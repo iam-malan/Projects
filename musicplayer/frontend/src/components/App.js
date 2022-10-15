69767becd319cc811f7cd5e185049742827711d4
iam-malan <64845039+iam-malan@users.noreply.github.com>
2022-10-15 19:54:19 +0200

    Frontend React working

--- musicplayer/frontend/src/components/App.js
+++ musicplayer/frontend/src/components/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-import HomePage from "./HomePage";
 
 export default class App extends Component {
   constructor(props) {
@@ -8,11 +7,7 @@ export default class App extends Component {
   }
 
   render() {
-    return (
-      <div>
-        <HomePage />
-      </div>
-    );
+    return <h1>Testing react</h1>;
   }
 }
 
